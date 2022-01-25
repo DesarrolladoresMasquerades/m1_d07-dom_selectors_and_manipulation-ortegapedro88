14f405c8445c31b998f87c75c3e9546634f73f0a
Pedro Ortega Escande <ortegapedro88@gmail.com>
2022-01-25 13:51:52 +0100

    exe 01 dom

--- exercises/01-exe-dom_selectors/scripts/scripts.js
+++ exercises/01-exe-dom_selectors/scripts/scripts.js
@@ -1,4 +1,5 @@
 // 1: Get the node with the  main title
+console.log(document.querySelector("h1"))
 
 // ... your code here
 
@@ -7,12 +8,16 @@
 
 // 2: Get the main title text
 
+console.log(document.querySelector("h1").innerText)
+
+
 // ... your code here
 
 // => Fruits and veggies
 // ----------------------------------------------
 
 // 3: Get all the nodes with the fruit items
+console.log(document.getElementsByClassName("fruit-item"))
 
 // ... your code here
 
@@ -21,12 +26,17 @@
 
 // 4: Get all the veggie items - the text
 
+Array.from(document.getElementsByClassName("veggie-item")).forEach(elem=>{console.log(elem.innerText)})
+
+
 // ... your code here
 
 // Broccoli Celery Potato Spinach
 // ----------------------------------------------
 
 // 5: Get the subtitle in the veggies section
+
+console.log("subtitlte: ",document.querySelectorAll("h2")[1].innerText)
 
 // ... your code here
 
@@ -35,12 +45,18 @@
 
 // 6: Add an ID 'fruit-title' to the first subtitle (in the Fruit section)
 
+document.querySelector("#container h2").id = "fruit-title"
+
 // ... your code here
 
 // => fruit-title
 // ----------------------------------------------
 
 // 7: Add a class 'best-fruit' to the 1st and the 3rd fruit
+
+document.querySelectorAll(".fruit-item")[0].classList.add ("best-fruit")
+document.querySelectorAll(".fruit-item")[2].classList.add ("best-fruit")
+
 
 // ... your code here
 
@@ -51,12 +67,24 @@
 
 // 8: Remove class 'veggies' from the unordered list of veggies
 
+const veggiesPre =document.querySelector(".veggies")
+veggiesPre.classList.remove ("veggies")
+veggiesPre.classList.add ("manuelita")
+
+
+
+
+
 // ... your code here
 
 // => <ul>...</ul>
 // ----------------------------------------------
 
 // 9: Add a class 'veggie-love' to all the veggies
+document.querySelectorAll(".veggie-item").forEach(elem=>{elem.classList.add ("veggie-love")})
+
+
+
 
 // ... your code here
 
@@ -64,6 +92,11 @@
 // ----------------------------------------------
 
 // 10: Remove class 'veggie-item' from 'spinach'
+
+document.querySelectorAll(".veggie-item").forEach(elem=>{
+    if(elem.innerText === "Spinach") {elem.classList.remove ("veggie-item")}
+})
+
 
 // ... your code here
 
